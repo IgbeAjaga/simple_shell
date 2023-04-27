a6a201f1229066c77c73ca8f83d65c2a046a8885
olumoyin joshua titobi <olumoyinjoshua19609@gmal.com>
2023-04-26 19:58:07 -0700

    new pid_max commit

--- pid_max.sh
+++ pid_max.sh
@@ -1,8 +1,2 @@
-#!/bin/bash
-
-# Get the maximum value of  process ID in /proc/sys/kernel/pid_max
-pid_max=$(cat /proc/sys/kernel/pid_max)
-
-# Print the maximum value
-echo "The maximum value for a process ID is: $pid_max"
-
+#!/bin/sh
+cat /proc/sys/kernel/pid_max
